0991948768194fc205713e863193ae46303afb95
malireddy <malireddy@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-14 13:15:03 +0000

    Made changes to hide wifi details if wifi   present is NO
    git-svn-id: http://192.168.2.90/svn/bartsy-venue-android/Venue@19 c3b44e55-a100-42f6-9c86-32dbf579054b

--- src/com/vendsy/bartsy/venue/VenueRegistrationActivity.java
+++ src/com/vendsy/bartsy/venue/VenueRegistrationActivity.java
@@ -3,10 +3,6 @@ package com.vendsy.bartsy.venue;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.vendsy.bartsy.venue.R;
-import com.vendsy.bartsy.venue.utils.Constants;
-import com.vendsy.bartsy.venue.utils.WebServices;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -18,16 +14,23 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.Toast;
 
-public class VenueRegistrationActivity extends Activity implements OnClickListener {
+import com.vendsy.bartsy.venue.utils.Constants;
+import com.vendsy.bartsy.venue.utils.WebServices;
+
+public class VenueRegistrationActivity extends Activity implements
+		OnClickListener {
 
 	private EditText locuId, paypal, wifiName, wifiPassword;
 	private RadioGroup typeOfAuthentication, wifiPresent;
-	
+
 	private Handler handler = new Handler();
+	private LinearLayout wifiNameLinear, wifiTypeLinear, wifiPasswordLinear;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -41,19 +44,47 @@ public class VenueRegistrationActivity extends Activity implements OnClickListen
 		typeOfAuthentication = (RadioGroup) findViewById(R.id.authentication);
 		wifiPresent = (RadioGroup) findViewById(R.id.wifiPresent);
 
+		wifiNameLinear = (LinearLayout) findViewById(R.id.wifiNameLinear);
+		wifiTypeLinear = (LinearLayout) findViewById(R.id.wifiTypeLinear);
+		wifiPasswordLinear = (LinearLayout) findViewById(R.id.wifiPasswordLinear);
+
 		// Setup a listener for the submit button
-		findViewById(R.id.button_venue_registration_submit).setOnClickListener(this);
+		findViewById(R.id.button_venue_registration_submit).setOnClickListener(
+				this);
+
+		wifiPresent.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				// TODO Auto-generated method stub
+
+				RadioButton selectedButton = (RadioButton) findViewById(checkedId);
+				String name = selectedButton.getText().toString();
+				if (name.equalsIgnoreCase("No")) {
+					wifiNameLinear.setVisibility(View.GONE);
+					wifiTypeLinear.setVisibility(View.GONE);
+					wifiPasswordLinear.setVisibility(View.GONE);
+				} else
+
+				{
+					wifiNameLinear.setVisibility(View.VISIBLE);
+					wifiTypeLinear.setVisibility(View.VISIBLE);
+					wifiPasswordLinear.setVisibility(View.VISIBLE);
+				}
+
+			}
+		});
 	}
 
 	@Override
 	public void onClick(View arg0) {
-//		Intent intent = new Intent(this, MainActivity.class);
+		// Intent intent = new Intent(this, MainActivity.class);
 
 		// Perform registration - for now assume all will go well
 		registrationAction();
 		Log.d("Bartsy", "Clicked on submit button");
 	}
-	
+
 	public void registrationAction() {
 
 		int selectedWifiPresent = wifiPresent.getCheckedRadioButtonId();
@@ -77,12 +108,14 @@ public class VenueRegistrationActivity extends Activity implements OnClickListen
 			postData.put("deviceToken", deviceToken);
 			postData.put("wifiName", wifiName.getText().toString());
 			postData.put("wifiPassword", wifiPassword.getText().toString());
-			postData.put("typeOfAuthentication", typeOfAuthentication == null ? "" :
-				typeOfAuthentication.getText().toString());
+			postData.put("typeOfAuthentication",
+					typeOfAuthentication == null ? "" : typeOfAuthentication
+							.getText().toString());
 			postData.put("paypalId", paypal.getText().toString());
 			postData.put("deviceType", "0");
 
-			if (wifi == null? false : wifi.getText().toString().equalsIgnoreCase("Yes"))
+			if (wifi == null ? false : wifi.getText().toString()
+					.equalsIgnoreCase("Yes"))
 				postData.put("wifiPresent", "1");
 			else
 				postData.put("wifiPresent", "0");
@@ -105,37 +138,54 @@ public class VenueRegistrationActivity extends Activity implements OnClickListen
 
 					if (response != null) {
 						final JSONObject json = new JSONObject(response);
-						
+
 						handler.post(new Runnable() {
-							
+
 							@Override
 							public void run() {
 								try {
-									int errorCode = Integer.parseInt(json.getString("errorCode"));
-									String errorMessage = json.getString("errorMessage");
+									int errorCode = Integer.parseInt(json
+											.getString("errorCode"));
+									String errorMessage = json
+											.getString("errorMessage");
 									String venueName = null, venueId = null;
-									Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_LONG).show();
+									Toast.makeText(getApplicationContext(),
+											errorMessage, Toast.LENGTH_LONG)
+											.show();
 									BartsyApplication app;
-									switch(errorCode) {
-									case 2: 
-										// venue already exists - still save the profile locally for now
+									switch (errorCode) {
+									case 2:
+										// venue already exists - still save the
+										// profile locally for now
 										venueName = "Chaya Venice";
 										venueId = "5a0999dda39f9fe07a44";
-									case 0: 
-										// Save the venue id in shared preferences
-										venueId = venueId == null ? json.getString("venueId") : venueId;
-										venueName = venueName == null? json.getString("venueName") : venueName;
-									    SharedPreferences sharedPref = getSharedPreferences(getResources().getString(R.string.config_shared_preferences_name), Context.MODE_PRIVATE);
-										SharedPreferences.Editor editor = sharedPref.edit();
-										editor.putString("RegisteredVenueId", venueId);
-										editor.putString("RegisteredVenueName", venueName);
-										app = (BartsyApplication)getApplication();
+									case 0:
+										// Save the venue id in shared
+										// preferences
+										venueId = venueId == null ? json
+												.getString("venueId") : venueId;
+										venueName = venueName == null ? json
+												.getString("venueName")
+												: venueName;
+										SharedPreferences sharedPref = getSharedPreferences(
+												getResources()
+														.getString(
+																R.string.config_shared_preferences_name),
+												Context.MODE_PRIVATE);
+										SharedPreferences.Editor editor = sharedPref
+												.edit();
+										editor.putString("RegisteredVenueId",
+												venueId);
+										editor.putString("RegisteredVenueName",
+												venueName);
+										app = (BartsyApplication) getApplication();
 										app.venueProfileID = venueId;
 										app.venueProfileName = venueName;
-										
+
 										editor.commit();
 
-										Intent intent = new Intent(VenueRegistrationActivity.this,
+										Intent intent = new Intent(
+												VenueRegistrationActivity.this,
 												MainActivity.class);
 										intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 										startActivity(intent);
@@ -151,10 +201,10 @@ public class VenueRegistrationActivity extends Activity implements OnClickListen
 									// TODO Auto-generated catch block
 									e.printStackTrace();
 								}
-								
+
 							}
 						});
-					
+
 					}
 
 				} catch (Exception e) {
