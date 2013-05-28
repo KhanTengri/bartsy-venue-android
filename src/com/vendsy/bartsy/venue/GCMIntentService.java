311649932b1490c93624d0c21fe76717ae19f213
malireddy <malireddy@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-28 13:09:13 +0000

    Handling heart beat Push notification
    git-svn-id: http://192.168.2.90/svn/bartsy-venue-android/Venue@54 c3b44e55-a100-42f6-9c86-32dbf579054b

--- src/com/vendsy/bartsy/venue/GCMIntentService.java
+++ src/com/vendsy/bartsy/venue/GCMIntentService.java
@@ -27,6 +27,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -36,7 +37,9 @@ import com.google.android.gcm.GCMBaseIntentService;
 import com.google.android.gcm.GCMRegistrar;
 import com.vendsy.bartsy.venue.model.Order;
 import com.vendsy.bartsy.venue.model.Profile;
+import com.vendsy.bartsy.venue.utils.Constants;
 import com.vendsy.bartsy.venue.utils.Utilities;
+import com.vendsy.bartsy.venue.utils.WebServices;
 
 //import static com.vendsy.bartsy.venue.utils.Utilities.displayMessage;
 
@@ -48,6 +51,7 @@ public class GCMIntentService extends GCMBaseIntentService {
 
 	@SuppressWarnings("hiding")
 	private static final String TAG = "GCMIntentService";
+	// Setup application pointer
 
 	public GCMIntentService() {
 
@@ -75,8 +79,7 @@ public class GCMIntentService extends GCMBaseIntentService {
 		if (GCMRegistrar.isRegisteredOnServer(getApplicationContext())) {
 			// ServerUtilities.unregister(context, registrationId);
 		} else {
-			// This callback results from the call to unregister made on
-			// ServerUtilities when the registration to the server failed.
+			// This callback results from the call to unregister made on ServerUtilities when the registration to the server failed.
 			Log.i(TAG, "Ignoring unregister callback");
 		}
 	}
@@ -87,15 +90,18 @@ public class GCMIntentService extends GCMBaseIntentService {
 	 * @param message
 	 * @return
 	 */
-	private String processPushNotification(String message) {
+	private String processPushNotification(JSONObject json) {
 		BartsyApplication app = (BartsyApplication) getApplication();
 		String messageTypeMSG = "";
 		try {
-			Log.i(TAG, "Push notification resposne :: " + message);
-			JSONObject json = new JSONObject(message);
+			Log.i(TAG, "Push notification resposne :: " + json);
+			// JSONObject json = new JSONObject(message);
 			if (json.has("messageType")) {
 				// To check push notification message type
-				if (json.getString("messageType").equals("placeOrder")) {
+				if (json.getString("messageType").equalsIgnoreCase("heartBeat")) {
+					// Handling heart beat webservice
+					handlingHeartBeat(app);
+				} else if (json.getString("messageType").equals("placeOrder")) {
 					Order order = new Order(json);
 					// Add order to order list
 					app.addOrder(order);
@@ -110,15 +116,14 @@ public class GCMIntentService extends GCMBaseIntentService {
 				} else if (json.getString("messageType").equals("orderTimeout")) {
 					JSONArray cancelledOrders = json.has("ordersCancelled") ? json
 							.getJSONArray("ordersCancelled") : null;
-							// When user checkout from venue, it is required to remove  user open orders
-							
-							if (cancelledOrders != null && cancelledOrders.length() > 0) {
-								// For removing the cancelled orders
-								app.removeOrders(cancelledOrders);
+					// When user checkout from venue, it is required to remove user open orders
+					if (cancelledOrders != null && cancelledOrders.length() > 0) {
+						// For removing the cancelled orders
+						app.removeOrders(cancelledOrders);
 
-							}
-							// To display message in PN
-							messageTypeMSG = "Order time out";
+					}
+					// To display message in PN
+					messageTypeMSG = "Order time out";
 				}
 
 				else if (json.getString("messageType").equals("userCheckOut")) {
@@ -126,8 +131,7 @@ public class GCMIntentService extends GCMBaseIntentService {
 					app.removePerson(json.getString("bartsyId"));
 					JSONArray cancelledOrders = json.has("cancelledOrders") ? json
 							.getJSONArray("cancelledOrders") : null;
-					// When user checkout from venue, it is required to remove  user open orders
-					
+					// When user checkout from venue, it is required to remove user open orders
 					if (cancelledOrders != null && cancelledOrders.length() > 0) {
 						// For removing the cancelled orders
 						app.removeOrders(cancelledOrders);
@@ -144,6 +148,34 @@ public class GCMIntentService extends GCMBaseIntentService {
 		return messageTypeMSG;
 	}
 
+	// HeartBeat web service calling 
+	private void handlingHeartBeat(final BartsyApplication app) {
+
+		Log.i(TAG, "handlingHeartBeat");
+		final Context context = getApplicationContext();
+
+		
+		new Thread() {
+		public void run() {
+			Log.i(TAG, "In thread");
+			// Checking venue id is null or not
+				if (app.venueProfileID!=null) {
+					try {
+						// Created jsonobject
+						JSONObject postData = new JSONObject();
+						postData.put("venueId", app.venueProfileID);
+						// Heart beat Webservice calling
+						WebServices.postRequest(Constants.URL_HEART_BEAT_VENUE,
+								postData, context);
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+		};
+		}.start();
+	}
+
 	@Override
 	protected void onMessage(Context context, Intent intent) {
 		Log.i(TAG, "Received message");
@@ -154,16 +186,27 @@ public class GCMIntentService extends GCMBaseIntentService {
 		String count = (String) intent.getExtras().get("badgeCount");
 		Uri notification = RingtoneManager
 				.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-		Ringtone ringtone = RingtoneManager.getRingtone(context, notification);
-		if (ringtone != null) {
-			ringtone.play();
-		}
 		Log.i(TAG, "message: " + message);
+		Ringtone ringtone = RingtoneManager.getRingtone(context, notification);
 		String notifyMSG = "Received..";
 		if (message == null) {
 			message = "";
 		} else {
-			notifyMSG = processPushNotification(message);
+			try {
+				JSONObject json = new JSONObject(message);
+				if (json.has("messageType"))
+
+					if (ringtone != null
+							&& !json.getString("messageType").equalsIgnoreCase(
+									"heartBeat")) {
+						ringtone.play();
+					}
+
+				notifyMSG = processPushNotification(json);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 		// displayMessage(context, message);
