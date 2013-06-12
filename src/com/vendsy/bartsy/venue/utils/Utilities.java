cd94a0d81069c8dc2eb503a2fc3c42410fa2b731
malireddy <malireddy@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-06-12 11:07:03 +0000

    Change in GCM Push notification
    git-svn-id: http://192.168.2.90/svn/bartsy-venue-android/Venue@73 c3b44e55-a100-42f6-9c86-32dbf579054b

--- src/com/vendsy/bartsy/venue/utils/Utilities.java
+++ src/com/vendsy/bartsy/venue/utils/Utilities.java
@@ -43,8 +43,8 @@ public final class Utilities {
 	/**
 	 * Google API project id registered to use GCM.
 	 */
-//	public static final String SENDER_ID = "227827031375";
-	public static final String SENDER_ID = "569714356856"; //public server
+//	public static final String SENDER_ID = "263062314156";
+	public static final String SENDER_ID = "605229245886"; //public server
 	
 	
 	/**
