b2f6275bb0562fd8dbd022ac282546519823e401
malireddy <malireddy@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-08-16 16:53:26 +0000

    JSON parsing  and setting all the fields to the venue object
    git-svn-id: http://192.168.2.90/svn/bartsy-venue-android/Venue@185 c3b44e55-a100-42f6-9c86-32dbf579054b

--- src/com/vendsy/bartsy/venue/model/Venue.java
+++ src/com/vendsy/bartsy/venue/model/Venue.java
@@ -1,21 +1,70 @@
 package com.vendsy.bartsy.venue.model;
 
-public class Venue {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-	private String id; // Unique key which is generated in the server
+import com.google.analytics.tracking.android.Log;
+import com.vendsy.bartsy.venue.R;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+/**
+ * 
+ * @author Seenu Malireddy
+ *
+ */
+public class Venue {
+	
+	private String id;
 	private String name;
+	private String imagePath;
 	private String latitude;
 	private String longitude;
+	private String venueStatus;
+	private String wifiPresent;
+	private String wifiName;
+	private String wifiPassword;
+	private String typeOfAuthentication;
 	private String address;
-
-	public String getAddress() {
-		return address;
+	private String cancelOrderTime;
+	private String totalTaxRate;
+	private String currentTime;
+	private String wifiNetworkType;
+	private String tableOrdering;
+	
+	//TODO We are not getting all the fields from the server
+	
+	
+	/**
+	 * TODO - Constructors / parsers
+	 */
+	
+	public Venue(JSONObject json) throws JSONException{
+		
+		// Parse all the details from the JSON format
+		name = json.getString("venueName");
+		id = json.getString("venueId");
+		imagePath = json.getString("venueImagePath");
+		latitude = json.getString("latitude");
+		longitude = json.getString("longitude");
+		venueStatus = json.getString("venueStatus");
+		wifiPresent = json.getString("wifiPresent");
+		wifiName = json.getString("wifiName");
+		wifiPassword = json.getString("wifiPassword");
+		typeOfAuthentication = json.getString("typeOfAuthentication");
+		address = json.getString("address");
+		cancelOrderTime = json.getString("cancelOrderTime");
+		totalTaxRate = json.getString("totalTaxRate");
+		currentTime = json.getString("currentTime");
+		wifiNetworkType = json.getString("wifiNetworkType");
+		tableOrdering = json.getString("tableOrdering");
+	};
+	
+	public boolean has(String field) {
+		return !(field == null || field.equals(""));
 	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
+	
 	public String getId() {
 		return id;
 	}
@@ -32,6 +81,14 @@ public class Venue {
 		this.name = name;
 	}
 
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
 	public String getLatitude() {
 		return latitude;
 	}
@@ -46,6 +103,100 @@ public class Venue {
 
 	public void setLongitude(String longitude) {
 		this.longitude = longitude;
+	}
+
+	public String getVenueStatus() {
+		return venueStatus;
+	}
+
+	public void setVenueStatus(String venueStatus) {
+		this.venueStatus = venueStatus;
+	}
+
+	public String getWifiPresent() {
+		return wifiPresent;
+	}
+
+	public void setWifiPresent(String wifiPresent) {
+		this.wifiPresent = wifiPresent;
+	}
+
+	public String getWifiName() {
+		return wifiName;
+	}
+
+	public void setWifiName(String wifiName) {
+		this.wifiName = wifiName;
+	}
+
+	public String getWifiPassword() {
+		return wifiPassword;
+	}
+
+	public void setWifiPassword(String wifiPassword) {
+		this.wifiPassword = wifiPassword;
+	}
+
+	public String getTypeOfAuthentication() {
+		return typeOfAuthentication;
+	}
+
+	public void setTypeOfAuthentication(String typeOfAuthentication) {
+		this.typeOfAuthentication = typeOfAuthentication;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCancelOrderTime() {
+		return cancelOrderTime;
+	}
+
+	public void setCancelOrderTime(String cancelOrderTime) {
+		this.cancelOrderTime = cancelOrderTime;
+	}
+
+	public String getTotalTaxRate() {
+		return totalTaxRate;
+	}
+
+	public void setTotalTaxRate(String totalTaxRate) {
+		this.totalTaxRate = totalTaxRate;
+	}
+
+
+	public String getCurrentTime() {
+		return currentTime;
+	}
+
+
+	public void setCurrentTime(String currentTime) {
+		this.currentTime = currentTime;
+	}
+
+
+	public String getWifiNetworkType() {
+		return wifiNetworkType;
+	}
+
+
+	public void setWifiNetworkType(String wifiNetworkType) {
+		this.wifiNetworkType = wifiNetworkType;
+	}
+
+
+	public String getTableOrdering() {
+		return tableOrdering;
+	}
+
+
+	public void setTableOrdering(String tableOrdering) {
+		this.tableOrdering = tableOrdering;
 	}
 
 }
