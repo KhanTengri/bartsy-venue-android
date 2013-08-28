4eee02a810466b7258cb0283c1e06eb2688814c9
malireddy <malireddy@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-08-28 15:47:47 +0000

    parsing the venue details from getVenueDetails sys call
    git-svn-id: http://192.168.2.90/svn/bartsy-venue-android/Venue@217 c3b44e55-a100-42f6-9c86-32dbf579054b

--- src/com/vendsy/bartsy/venue/model/Venue.java
+++ src/com/vendsy/bartsy/venue/model/Venue.java
@@ -2,13 +2,6 @@ package com.vendsy.bartsy.venue.model;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import com.google.analytics.tracking.android.Log;
-import com.vendsy.bartsy.venue.R;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.EditText;
 /**
  * 
  * @author Seenu Malireddy
@@ -31,9 +24,22 @@ public class Venue {
 	private String totalTaxRate;
 	private String currentTime;
 	private String wifiNetworkType;
-	private String tableOrdering;
-	
-	//TODO We are not getting all the fields from the server
+	private boolean tableOrdering;
+	private boolean isPickupLocution;
+	private String hours;
+	private String managerName;
+	private String managerEmail;
+	private String managerPassword;
+	private String managerCell;
+	private String representativeName;
+	private String representativeEmail;
+	private String representativeCell;
+	private String locuId;
+	private String venueImagePath;
+	private String locuUsername;
+	private String locuPassword;
+	private String locuSection;
+	private String phone;
 	
 	
 	/**
@@ -42,28 +48,89 @@ public class Venue {
 	
 	public Venue(JSONObject json) throws JSONException{
 		
-		// Parse all the details from the JSON format
-		name = json.getString("venueName");
-		id = json.getString("venueId");
-		imagePath = json.getString("venueImagePath");
-		latitude = json.getString("latitude");
-		longitude = json.getString("longitude");
-		venueStatus = json.getString("venueStatus");
-		wifiPresent = json.getString("wifiPresent");
-		wifiName = json.getString("wifiName");
-		wifiPassword = json.getString("wifiPassword");
-		typeOfAuthentication = json.getString("typeOfAuthentication");
-		address = json.getString("address");
-		cancelOrderTime = json.getString("cancelOrderTime");
-		totalTaxRate = json.getString("totalTaxRate");
-		currentTime = json.getString("currentTime");
-		wifiNetworkType = json.getString("wifiNetworkType");
-		tableOrdering = json.getString("tableOrdering");
+		// Parse all the details from the JSON Object
+		if(json.has("venueName")){
+			name = json.getString("venueName");
+		}
+		if(json.has("venueId")){
+			id = json.getString("venueId");
+		}
+		if(json.has("venueImagePath")){
+			imagePath = json.getString("venueImagePath");
+		}
+		if(json.has("latitude")){
+			latitude = json.getString("latitude");
+		}
+		if(json.has("longitude")){
+			longitude = json.getString("longitude");
+		}
+		if(json.has("locuId")){
+			locuId = json.getString("locuId");
+		}
+		if(json.has("venueStatus")){
+			venueStatus = json.getString("venueStatus");
+		}
+		if(json.has("wifiPresent")){
+			wifiPresent = json.getString("wifiPresent");
+		}
+		if(json.has("wifiName")){
+			wifiName = json.getString("wifiName");
+		}
+		if(json.has("wifiPassword")){
+			wifiPassword = json.getString("wifiPassword");
+		}
+		if(json.has("typeOfAuthentication")){
+			typeOfAuthentication = json.getString("typeOfAuthentication");
+		}
+		if(json.has("address")){
+			address = json.getString("address");
+		}
+		if(json.has("cancelOrderTime")){
+			cancelOrderTime = json.getString("cancelOrderTime");
+		}
+		if(json.has("totalTaxRate")){
+			totalTaxRate = json.getString("totalTaxRate");
+		}
+		if(json.has("currentTime")){
+			currentTime = json.getString("currentTime");
+		}
+		if(json.has("wifiNetworkType")){
+			wifiNetworkType = json.getString("wifiNetworkType");
+		}
+		if(json.has("tableOrdering")){
+			tableOrdering = json.getBoolean("tableOrdering");
+		}
+		if(json.has("venueLogin")){
+			managerEmail = json.getString("venueLogin");
+		}
+		if(json.has("open_hours")){
+			hours = json.getString("open_hours");
+		}
+		if(json.has("venueImagePath")){
+			venueImagePath = json.getString("venueImagePath");
+		}
+		if(json.has("isPickupLocution")){
+			isPickupLocution = json.getBoolean("isPickupLocution");
+		}
+		if(json.has("locuId")){
+			locuId = json.getString("locuId");
+		}
+		if(json.has("locuUsername")){
+			locuUsername = json.getString("locuUsername");
+		}
+		if(json.has("locuPassword")){
+			locuPassword = json.getString("locuPassword");
+		}
+		if(json.has("phone")){
+			phone = json.getString("phone");
+		}
+		if(json.has("venuePassword")){
+			managerPassword = json.getString("venuePassword");
+		}
+		if(json.has("locuSection")){
+			locuSection = json.getString("locuSection");
+		}
 	};
-	
-	public boolean has(String field) {
-		return !(field == null || field.equals(""));
-	}
 	
 	public String getId() {
 		return id;
@@ -190,13 +257,132 @@ public class Venue {
 	}
 
 
-	public String getTableOrdering() {
+	public boolean getTableOrdering() {
 		return tableOrdering;
 	}
 
-
-	public void setTableOrdering(String tableOrdering) {
+	public void setTableOrdering(boolean tableOrdering) {
 		this.tableOrdering = tableOrdering;
 	}
 
+	public String getHours() {
+		return hours;
+	}
+
+	public void setHours(String hours) {
+		this.hours = hours;
+	}
+
+	public String getManagerName() {
+		return managerName;
+	}
+
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+
+	public String getManagerEmail() {
+		return managerEmail;
+	}
+
+	public void setManagerEmail(String managerEmail) {
+		this.managerEmail = managerEmail;
+	}
+
+	public String getManagerCell() {
+		return managerCell;
+	}
+
+	public void setManagerCell(String managerCell) {
+		this.managerCell = managerCell;
+	}
+
+	public String getRepresentativeName() {
+		return representativeName;
+	}
+
+	public void setRepresentativeName(String representativeName) {
+		this.representativeName = representativeName;
+	}
+
+	public String getRepresentativeEmail() {
+		return representativeEmail;
+	}
+
+	public void setRepresentativeEmail(String representativeEmail) {
+		this.representativeEmail = representativeEmail;
+	}
+
+	public String getRepresentativeCell() {
+		return representativeCell;
+	}
+
+	public void setRepresentativeCell(String representativeCell) {
+		this.representativeCell = representativeCell;
+	}
+
+	public boolean isPickupLocution() {
+		return isPickupLocution;
+	}
+
+	public void setPickupLocution(boolean isPickupLocution) {
+		this.isPickupLocution = isPickupLocution;
+	}
+
+	public String getLocuId() {
+		return locuId;
+	}
+
+	public void setLocuId(String locuId) {
+		this.locuId = locuId;
+	}
+
+	public String getVenueImagePath() {
+		return venueImagePath;
+	}
+
+	public void setVenueImagePath(String venueImagePath) {
+		this.venueImagePath = venueImagePath;
+	}
+
+	public String getLocuUsername() {
+		return locuUsername;
+	}
+
+	public void setLocuUsername(String locuUsername) {
+		this.locuUsername = locuUsername;
+	}
+
+	public String getLocuPassword() {
+		return locuPassword;
+	}
+
+	public void setLocuPassword(String locuPassword) {
+		this.locuPassword = locuPassword;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getManagerPassword() {
+		return managerPassword;
+	}
+
+	public void setManagerPassword(String managerPassword) {
+		this.managerPassword = managerPassword;
+	}
+
+	public String getLocuSection() {
+		return locuSection;
+	}
+
+	public void setLocuSection(String locuSection) {
+		this.locuSection = locuSection;
+	}
+	
 }
